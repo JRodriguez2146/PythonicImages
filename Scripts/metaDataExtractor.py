d4abc0fca39166d5ef33e9788fcf17279fc044a9
JRodriguez-Repo <97987743+JRodriguez-Repo@users.noreply.github.com>
2024-04-30 21:29:24 -0400

    Adding comments

--- Scripts/metaDataExtractor.py
+++ Scripts/metaDataExtractor.py
@@ -5,14 +5,16 @@ from PIL.ExifTags import TAGS
 img_file = r" **add image path here** "
 
 image = Image.open(img_file)
-
+#Dictionary to store image metadata
 exif = {}
+#Get all relevant data
 for tag, value in image.getexif().items():
     if tag in TAGS:
         exif[TAGS[tag]] = value
 
 print(exif)
 
+#Format geo coordinates
 if "GPSInfo" in exif:
     geo_coordinate = '{0} {1} {2:.2f} {3}, {4} {5} {6:.2f} {7}'.format(
         exif["GPSInfo"][2][0][0],
